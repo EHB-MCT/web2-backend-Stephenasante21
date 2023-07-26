58f8f8f995b94200157232cf18eaef330eeb9e76
Stephenasante21 <stephen.asante@student.ehb.be>
2023-07-27 00:13:00 +0200

    login

--- index.js
+++ index.js
@@ -1,45 +1,253 @@
-const { MongoClient } = require("mongodb");
- 
-// Replace the following with your Atlas connection string                                                                                                                                        
-const url = "mongodb+srv://stephenasa:stephenasa@cluster0.mgzdpzt.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(url);
- 
- // The database to use
- const dbName = "Web2courseproject";
-                      
- async function run() {
-    try {
-         await client.connect();
-         console.log("Connected correctly to server");
-         const db = client.db(dbName);
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const { MongoClient } = require('mongodb')
+const {v4: uuidv4, validate: uuidValidate} = require('uuid');
+require('dotenv').config()
 
-         // Use the collection "people"
-         const col = db.collection("images");
+//mongoclient
+const client = new MongoClient(process.env.FINAL_URL)
 
-         // Construct a document                                                                                                                                                              
-         let imagesDocument = {
-             img_url: "jesup_5",
-         }
+app.use(express.urlencoded({extended:false}));
+app.use(cors())
+app.use(express.json())
 
-         // Insert a single document, wait for promise so we can read it back
-         const p = await col.insertOne(imagesDocument);
-         // Find one document
-         const myDoc = await col.findOne();
-         // Print to the console
-         console.log(myDoc);
+app.get("/testMongo", async (req,res) => {
+    try{
+        //connect to the db
+        await client.connect();
 
-        } catch (err) {
-         console.log(err.stack);
-     }
- 
-     finally {
+        //retrieve collection data
+        const coll = client.db('Web2courseproject').collection('users');
+        const users = await coll.find({}).toArray();
+
+        //send back the data with the response
+        res.status(200).send(users);
+        }catch(error){
+            console.log(error)
+            res.status(500).send({
+            error: 'Something went wrong'
+        })
+    }finally{
         await client.close();
     }
-}
 
-run().catch(console.dir);
+})
 
+//REGISTER
+app.post("/register", async (req,res) => {
 
+    //check for empty fields 
+    //username
+    if(!req.body.username){
+        res.status(401).send({
+            status: "bad Request",
+            message: "some field is missing: username"
+        })
+        return
+    }
 
+    //email
+    if(!req.body.email){
+        res.status(401).send({
+            status: "bad Request",
+            message: "some field is missing: email"
+        })
+        return
+    }
 
-// login 
+    //password
+    if(!req.body.password){
+        res.status(401).send({
+            status: "bad Request",
+            message: "some field is missing: password"
+        })
+        return
+    }
+
+    //description
+    /*if(!req.body.description){
+        res.status(401).send({
+            status: "bad Request",
+            message: "some field is missing: description"
+        })
+    }*/
+
+    try{
+        // Connect to the database
+        await client.connect();
+
+        // Retrieve collection data
+        const coll = client.db('Web2courseproject').collection('users')
+
+        // Check if a user with the given email already exists
+        const existingUser = await coll.findOne({ email: req.body.email })
+
+        if (existingUser) {
+            res.status(409).send({
+                status: "ERROR",
+                message: "User with this email already exists."
+            })
+            return
+        }
+
+        // Create a new user object
+        const user = {
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        uuid: uuidv4()
+        }
+
+        // Insert the new user into the database
+        const insertedUser = await coll.insertOne(user);
+
+        // Send back a success response when the user is saved
+        res.status(201).send({
+        status: "OK SUCCES",
+        message: "User has been created",
+        data: insertedUser
+        })
+    }catch(error){
+        console.log(error)
+        res.status(500).send({
+            error: 'Something went wrong'
+        })
+    }finally{
+        await client.close();
+    }
+
+})
+
+//LOGIN
+app.post("/login", async (req,res) => {
+
+    //Check for empty fields
+    //email
+    if(!req.body.email){
+        res.status(401).send({
+            status: "bad Request",
+            message: "some field is missing: email"
+        })
+        return
+    }
+
+    //password
+    if(!req.body.password){
+        res.status(401).send({
+            status: "bad Request",
+            message: "some field is missing: password"
+        })
+        return
+    }
+
+    try{
+        //connect to the db
+        await client.connect();
+
+        const loginuser = {
+            email: req.body.email,
+            password: req.body.password,
+        }
+
+        //retrieve collection data
+        const coll = client.db('Web2courseproject').collection('users');
+
+        const query = {email: loginuser.email}
+        const findUser = await coll.findOne(query)
+
+        if(findUser){
+            //compare passwords
+            if(findUser.password == loginuser.password){
+                res.status(200).send({
+                    status: "OK SUCCES",
+                    message: "YOU ARE LOGGGED IN"
+                })
+            }else {
+                //password is incorrect
+                res.status(401).send({
+                    status: "ERROR",
+                    message: "Password is incorrect!!!!"
+                })
+            }
+        }else{
+            //no user found send back error
+            res.status(401).send({
+                status: "ERROR",
+                message: "no user with this email has been found!!!! Register first"
+            })
+        }
+
+    }catch(error){
+        console.log(error)
+        res.status(500).send({
+            error: 'Something went wrong',
+            value: error
+        })
+    }finally{
+        await client.close();
+    }
+
+})
+
+app.post("/veryfyID", async (req,res) => {
+
+    //Check for empty and faulty ID fields
+    if(!req.body.uuid){
+        res.status(401).send({
+            status: "bad Request",
+            message: "ID is missing"
+        })
+        return
+    }else{
+        if(!uuidValidate(req.body.uuid)){
+            res.status(401).send({
+                status: "bad Request",
+                message: "ID is not a valid UUID"
+            })
+            return
+        }
+    }
+
+    try{
+        //connect to the db
+        await client.connect();
+
+        //retrieve collection data
+        const coll = client.db('Web2courseproject').collection('users');
+
+        const query = {uuid: req.body.uuid}
+        const user = await coll.findOne(query)
+
+        if(user){
+            res.status(200).send({
+                status: "Verified",
+                message: "Your UUID is valid",
+                data: {
+                    username: user.username,
+                    email: user.email,
+                    uuid: user.uuid
+                }
+            })
+        }else{
+            //no user found send back error
+            res.status(401).send({
+                status: "verify ERROR",
+                message: `No user exists with id ${req.body.uuid}`
+            })
+        }
+
+    }catch(error){
+        console.log(error)
+        res.status(500).send({
+            error: 'Something went wrong',
+            value: error
+        })
+    }finally{
+        await client.close();
+    }
+
+})
+
+app.listen(3000);
+console.log("app running at http://localhost:3000");
